fbdf0fe18e265c2d7255ed22a0f028b4b5d7dbcc
mhashim6 <m6hashim@gmail.com>
2019-09-16 17:38:42 +0200

    added SpongeBob time cards sound effects.

--- src/Drumroll.kt
+++ src/Drumroll.kt
@@ -3,6 +3,7 @@ import com.intellij.openapi.compiler.CompileContext
 import com.intellij.openapi.compiler.CompilerTopics
 import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.project.Project
+import kotlin.random.Random.Default.nextBoolean
 
 /**
  *@author mhashim6 on 20/03/19
@@ -11,15 +12,20 @@ class Drumroll(private val project: Project) : ProjectComponent {
     private val messages by lazy { project.messageBus.connect() }
     private val success = arrayOf("Ba-Dum-Tss!", "applause")
     private val error = arrayOf("failure", "laughter", "booing")
+    private val timeCards = arrayOf("eternity_later", "moments_later", "pair_of_pants_later", "inches_later")
 
     override fun projectOpened() {
         messages.subscribe(CompilerTopics.COMPILATION_STATUS, object : CompilationStatusListener {
             override fun compilationFinished(aborted: Boolean, errors: Int, warnings: Int, compileContext: CompileContext) {
-                when {
-                    errors > 0 -> play(error.random())
-                    warnings > 0 -> play("metal_gear")
-                    else -> play(success.random())
-                }
+                play(
+                        if (nextBoolean())
+                            when {
+                                errors > 0 -> error.random()
+                                warnings > 0 -> "metal_gear"
+                                else -> success.random()
+                            }
+                        else timeCards.random()
+                )
             }
         })
     }
