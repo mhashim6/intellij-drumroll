e596e1aea608683af53b43cea0ab84f85e731d54
mhashim6 <m6hashim@gmail.com>
2019-09-30 19:44:23 +0200

    support for android gradle events.

--- src/Drumroll.kt
+++ src/Drumroll.kt
@@ -1,38 +1,45 @@
 import com.intellij.openapi.compiler.CompilationStatusListener
 import com.intellij.openapi.compiler.CompileContext
 import com.intellij.openapi.compiler.CompilerTopics
-import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.project.Project
 import kotlin.random.Random.Default.nextBoolean
 
 /**
  *@author mhashim6 on 20/03/19
  */
-class Drumroll(private val project: Project) : ProjectComponent {
+open class Drumroll(private val project: Project) {
+
+    enum class BuildState { Success, Error, Warning }
+
     private val messages by lazy { project.messageBus.connect() }
     private val success = arrayOf("Ba-Dum-Tss!", "applause")
     private val error = arrayOf("failure", "laughter", "booing")
     private val timeCards = arrayOf("eternity_later", "moments_later", "pair_of_pants_later", "inches_later")
 
-    override fun projectOpened() {
+    init {
         messages.subscribe(CompilerTopics.COMPILATION_STATUS, object : CompilationStatusListener {
             override fun compilationFinished(aborted: Boolean, errors: Int, warnings: Int, compileContext: CompileContext) {
-                play(
-                        if (nextBoolean())
-                            when {
-                                errors > 0 -> error.random()
-                                warnings > 0 -> "metal_gear"
-                                else -> success.random()
-                            }
-                        else timeCards.random()
+                notifyState(
+                        when {
+                            errors > 0 -> BuildState.Error
+                            warnings > 0 -> BuildState.Warning
+                            else -> BuildState.Success
+                        }
                 )
             }
         })
     }
 
-    override fun projectClosed() {
-        stop()
-        messages.disconnect()
+    fun notifyState(state: BuildState) {
+        play(
+                if (nextBoolean())
+                    when (state) {
+                        BuildState.Success -> success.random()
+                        BuildState.Error -> error.random()
+                        BuildState.Warning -> "metal_gear"
+                    }
+                else timeCards.random()
+        )
     }
 }
 
