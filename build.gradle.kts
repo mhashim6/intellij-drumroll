96289715acf1709a71536167a96de0afef15c480
Muhammad Hashim <msg@mhashim6.me>
2022-09-17 01:54:39 +0200

    Update plugin framework and dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    java
+    id("org.jetbrains.kotlin.jvm") version "1.6.20"
+    id("org.jetbrains.intellij") version "1.7.0"
+}
+
+group = "mhashim6.idea.plugin"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2021.3")
+    type.set("IC") // Target IDE Platform
+    pluginName.set("Drum-Roll") //TODO
+    plugins.set(listOf("java", "android"))
+}
+
+dependencies {
+    implementation(fileTree("lib"){
+        include("*.jar")
+    })
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("213")
+        untilBuild.set("223.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
