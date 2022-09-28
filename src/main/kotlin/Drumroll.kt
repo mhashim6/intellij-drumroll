f5d25e8bb8b49eb3b1e61b4b43ce5d48d055246a
Muhammad Hashim <msg@mhashim6.me>
2022-09-28 03:42:34 +0200

    listen for project and execution topics

--- src/main/kotlin/Drumroll.kt
+++ src/main/kotlin/Drumroll.kt
@@ -5,6 +5,9 @@ import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.diagnostic.Logger
 
 import com.intellij.openapi.project.Project
+import com.intellij.task.ProjectTaskContext
+import com.intellij.task.ProjectTaskListener
+import com.intellij.task.ProjectTaskManager
 
 
 /**
@@ -14,50 +17,55 @@ open class Drumroll(private val project: Project) {
     private val messages by lazy { project.messageBus.connect() }
 
     init {
-        messages.subscribe(ExecutionManager.EXECUTION_TOPIC, Listener())
+        messages.subscribe(ExecutionManager.EXECUTION_TOPIC, eventConsumer)
+        messages.subscribe(ProjectTaskListener.TOPIC, eventConsumer)
     }
 
     companion object {
-
+        private val eventConsumer = Listener()
         private val logger by lazy { Logger.getInstance("Drumroll") }
-        private val now: Long
-            get() = System.currentTimeMillis()
 
-        private var currentState = BuildState.None
-        private val stateUpdateStamp = mutableMapOf(
-            BuildState.Success to now,
-            BuildState.Error to now,
-            BuildState.Warning to now,
-            BuildState.None to now,
-        )
-
-        private inline fun debounce(state: BuildState, threshold: Int = 5000, block: () -> Unit) {
-            if (currentState == state && now - stateUpdateStamp[state]!! < threshold) return
-            stateUpdateStamp[state] = now
-            block()
-        }
+        private var playContext = Context.None
 
         private fun taskTypeOf(env: ExecutionEnvironment) = env.run {
             runnerAndConfigurationSettings?.configuration?.beforeRunTasks?.let {
                 logger.warn("before tasks: $it")
-                it.forEach { task ->
-                    logger.warn("task: ${task.providerId}\n")
-
-                }
             }
             runnerAndConfigurationSettings?.name ?: runProfile.name
         }.lowercase()
 
         private fun ExecutionEnvironment.isLengthyTask() =
-            taskTypeOf(this).contains("build") || taskTypeOf(this).contains("assemble")
+            taskTypeOf(this).also {
+                logger.warn("task type: ${it}\n")
+            }.containsAny("build", "assemble", "make", "beforeruntask")
 
-        class Listener : ExecutionListener {
+        class Listener : ExecutionListener, ProjectTaskListener {
+
+            override fun started(context: ProjectTaskContext) {
+                super.started(context)
+                logger.warn("context: ${context.runConfiguration?.name}\n")
+
+                notifyState(BuildState.Starting, Context.ProjectTask)
+            }
+
+            override fun finished(result: ProjectTaskManager.Result) {
+                super.finished(result)
+                logger.warn("result: ${result.context.runConfiguration?.name}\n")
+
+                notifyState(
+                    when {
+                        result.hasErrors() -> BuildState.Error
+                        else -> BuildState.Success
+                    },
+                    Context.ProjectTask
+                )
+            }
 
             override fun processStarted(executorId: String, env: ExecutionEnvironment, handler: ProcessHandler) {
                 super.processStarted(executorId, env, handler)
                 if (!env.isLengthyTask()) return
                 logger.warn("executor: $executorId :: ${env.executionTarget.displayName} :: ${taskTypeOf(env)}\n")
-                play("drumroll_faded")
+                notifyState(BuildState.Starting, Context.Execution)
             }
 
             override fun processTerminated(
@@ -66,11 +74,13 @@ open class Drumroll(private val project: Project) {
                 super.processTerminated(executorId, env, handler, exitCode)
                 if (!env.isLengthyTask()) return
                 logger.warn("executor: $executorId :: ${env.executionTarget.displayName} :: ${taskTypeOf(env)}\n")
+
                 notifyState(
                     when (exitCode) {
                         0 -> BuildState.Success
                         else -> BuildState.Error
-                    }
+                    },
+                    Context.Execution
                 )
             }
         }
@@ -79,15 +89,19 @@ open class Drumroll(private val project: Project) {
         private val success = arrayOf("Ba-Dum-Tss!", "wow", "boring") + timeCards
         private val error = arrayOf("failure", "doh", "oh_geez", "better_call_saul", "metal_gear")
 
-        fun notifyState(state: BuildState) {
-//            debounce(state) {
+        fun notifyState(state: BuildState, context: Context) {
+            if (isPlaying() && context != playContext) {
+                playContext = context
+                return
+            }
             play(
                 when (state) {
+                    BuildState.Starting -> "drumroll_faded"
                     BuildState.Success -> success.random()
                     else -> error.random()
                 }
             )
-//            }
+            playContext = context
         }
     }
 }
--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -4,18 +4,17 @@ import jaco.mp3.player.MP3Player
  *@author mhashim6 on 20/03/19
  */
 
-private val player = MP3Player()
-private var currentlyPlaying: String? = null
+private val player  by lazy { MP3Player() }
 
 fun play(what: String, extension: String = "mp3") {
-    if (what == currentlyPlaying) return
     if (!player.isStopped)
         player.stop()
     player.playList.clear()
     player.addToPlayList(Drumroll::class.java.getResource("$what.$extension"))
     player.play()
-    currentlyPlaying = what
 }
+
+fun isPlaying() = !player.run { isStopped || isPaused || !isEnabled }
 
 fun stop() {
     player.stop()
